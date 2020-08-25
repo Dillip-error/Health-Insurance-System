6653cb0212abc49136f5b3231d0749b8a40ddad0
Dillip kumar sahoo <dillipgit@gmail.com>
2020-08-25 09:10:38 +0530

    Plan Module FolderStucture

---
+++ src/main/java/com/ait/binding/PlanBinding.java
@@ -0,0 +1,17 @@
+package com.ait.binding;
+
+import java.util.Date;
+
+import lombok.Data;
+@Data
+public class PlanBinding {
+
+	private Integer planId;
+	private String planName;
+	private String planDescription;
+	private String startDate;
+	private String endDate;
+	private Date createdDate;
+	private Date updatedDate;
+	
+}
---
+++ src/main/java/com/ait/entity/PlanEntity.java
@@ -0,0 +1,51 @@
+package com.ait.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "PLAN_TABLE")
+@Entity
+public class PlanEntity {
+
+	@Id
+	@Column(name = "PLAN_ID")
+	@SequenceGenerator(name = "SEQUENCE_ID", sequenceName = "REG_ID_SEQ", allocationSize = 1)
+	@GeneratedValue(generator = "SEQUENCE_ID", strategy = GenerationType.SEQUENCE)
+	private Integer planId;
+	@Column(name = "PLAN_NAME")
+	private String planName;
+	@Column(name = "PLAN_DESCRIPTION")
+	private String planDescription;
+	@Column(name = "START_DATE")
+	private String startDate;
+	@Column(name = "END_DATE")
+	private String endDate;
+	@CreationTimestamp
+	@Temporal(TemporalType.DATE)
+	@Column(name = "CREATED_DATE",updatable = false)
+	private Date createdDate;
+	@UpdateTimestamp  
+	@Temporal(TemporalType.DATE)
+	@Column(name = "UPDATED_DATE",insertable = false)
+	private Date updatedDate;
+	
+}
